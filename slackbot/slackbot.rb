e80cf278632e9207e205dafde468fdf514166081
Michael Diender <michael.diender@shinesolutions.com>
2016-09-29 15:38:51 +1000

    use constent instead of class variable.

--- slackbot/slackbot.rb
+++ slackbot/slackbot.rb
@@ -7,25 +7,23 @@ require 'ruby_aem'
 
 class Slackbot < Sinatra::Base
 
-  @@environment = 'development'
-
-  @@config = YAML::load_file(File.join(__dir__, 'config/config.yml'))
-
-  slackbot_config = {
-      'channel' => @@config[@@environment]['slackbotsy']['channel'],
-      'name' => @@config[@@environment]['slackbotsy']['name'],
-      'incoming_webhook' => @@config[@@environment]['slackbotsy']['incoming_webhook'],
-      'outgoing_token' => @@config[@@environment]['slackbotsy']['outgoing_token']
-  }
+  CONFIG = YAML::load_file(File.join(__dir__, 'config/config.yml'))['development']
 
   aem = RubyAem::Aem.new({
-                             :username => @@config[@@environment]['aem']['username'],
-                             :password => @@config[@@environment]['aem']['password'],
+                             :username => CONFIG['aem']['username'],
+                             :password => CONFIG['aem']['password'],
                              :protocol => 'http',
-                             :host => @@config[@@environment]['aem']['host'],
+                             :host => CONFIG['aem']['host'],
                              :port => 4502,
                              :debug => false
                          })
+
+  slackbot_config = {
+      'channel' => CONFIG['slackbotsy']['channel'],
+      'name' => CONFIG['slackbotsy']['name'],
+      'incoming_webhook' => CONFIG['slackbotsy']['incoming_webhook'],
+      'outgoing_token' => CONFIG['slackbotsy']['outgoing_token']
+  }
 
   bot = Slackbotsy::Bot.new(slackbot_config) do
 
@@ -33,11 +31,11 @@ class Slackbot < Sinatra::Base
 
       login = "#{mdata[1]}"
 
-      group_id = "#{@@config[@@environment]['aem']['default_group']}"
+      group_id = "#{Slackbot::CONFIG['aem']['default_group']}"
 
       user = aem.user("/home/users/#{login[0]}", "#{login}")
 
-      user_create_result = user.create(@@config[@@environment]['aem']['default_password'])
+      user_create_result = user.create(Slackbot::CONFIG['aem']['default_password'])
 
       add_to_group_result = user.add_to_group("/home/groups/#{group_id[0]}/", group_id)
 
